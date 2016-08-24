64f49cfc45bc132e1c7b51f49df17ede2477062c
CJPoll <CJPoll@gmail.com>
2016-08-23 18:16:09 -0600

    Have application ready

--- lib/ecto_meetup.ex
+++ lib/ecto_meetup.ex
@@ -1,2 +1,13 @@
 defmodule EctoMeetup do
+  use Application
+
+  def start(_, _) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      #supervisor(EctoMeetup.Repo, [])
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
 end
